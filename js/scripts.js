af9efbeae27a51394c762d287987e5956b0916c7
Nonyko <isis.nataly@hotmail.com>
2019-08-07 09:25:23 -0300

    ajustes finais

--- js/scripts.js
+++ js/scripts.js
@@ -26,8 +26,35 @@ $(document).ready(function(){
       cssEase: 'linear',
       slidesToShow: 5,
       slidesToScroll: 1,
-      adaptiveHeight: true
+      adaptiveHeight: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
   });
-  
   
